b51e8b2a6bf4c8c90f14add5cb4f7634ba6c6b2e
IastrebovVladimir <159059876+IastrebovVladimir@users.noreply.github.com>
2026-06-21 00:32:48 +0300

    Update SequenceReadOnlyStream.h

--- SequenceStream/SequenceReadOnlyStream.h
+++ SequenceStream/SequenceReadOnlyStream.h
@@ -12,13 +12,14 @@ template <typename T>
 class SequenceReadOnlyStream : public ReadOnlyStream<T> {
 private:
     Sequence<T>* sequence;
+    Enumerator<T>* enumerator;
     size_t position;
     bool isOpen;
     bool atEnd;
 
 public:
     explicit SequenceReadOnlyStream(Sequence<T>* sequence)
-            : sequence(sequence), position(0), isOpen(false), atEnd(false) {}
+            : sequence(sequence), enumerator(nullptr), position(0), isOpen(false), atEnd(false) {}
 
     bool IsEndOfStream() const override {
         return atEnd;
@@ -26,25 +27,25 @@ public:
 
     T Read() override {
         if (!isOpen) {
-            throw std::logic_error("Stream is not open");
+            throw std::logic_error("SequenceReadOnlyStream: stream is not open");
         }
         if (atEnd) {
             throw std::runtime_error("End of stream");
         }
-
-        auto* arr = dynamic_cast<ArraySequence<T>*>(sequence);
-        if (!arr) {
-            throw std::logic_error("SequenceReadOnlyStream expects ArraySequence");
+        if (sequence == nullptr) {
+            throw std::logic_error("SequenceReadOnlyStream: sequence is null");
+        }
+        if (enumerator == nullptr) {
+            throw std::logic_error("SequenceReadOnlyStream: enumerator is not initialized");
         }
 
-        if (position >= static_cast<size_t>(arr->GetLength())) {
+        if (!enumerator->MoveNext()) {
             atEnd = true;
             throw std::runtime_error("End of stream");
         }
 
-        T value = arr->Get(static_cast<int>(position++));
-        // после успешного чтения atEnd остаётся false
-        return value;
+        position++;
+        return enumerator->Current();
     }
 
     size_t GetPosition() const override {
@@ -64,16 +65,25 @@ public:
     }
 
     void Open() override {
-        if (!sequence) {
-            throw std::logic_error("Sequence is null");
+        if (sequence == nullptr) {
+            throw std::logic_error("SequenceReadOnlyStream: sequence is null");
         }
-        isOpen = true;
+
+        delete enumerator;
+        enumerator = new Enumerator<T>(sequence->GetEnumerator());
+
         position = 0;
+        isOpen = true;
         atEnd = false;
     }
 
     void Close() override {
         isOpen = false;
+        atEnd = false;
+        position = 0;
+
+        delete enumerator;
+        enumerator = nullptr;
     }
 };
 
